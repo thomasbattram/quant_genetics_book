319e3d5f4d6fe482a5e82e036ad04c8f774b13f0
Thomas Battram <tb13101@bristol.ac.uk>
2020-04-03 11:39:45 +0100

    added simulations for differential selection

--- code/chapter3.R
+++ code/chapter3.R
@@ -1,5 +1,5 @@
 
-
+library(MASS)
 
 # ------------------------------------------
 # covariance
@@ -50,5 +50,61 @@ cor_func <- function(x, y) {
 cor_func(x, y) # -0.05546129
 cor(x, y) # -0.05546129
 # huzza again! 
+
+# ------------------------------------------
+# differential selection 
+# ------------------------------------------
+
+generate_phens <- function(dad_phen, mum_phen, par_off_correlation) {
+	midparent <- (dad_phen + mum_phen) / 2
+	n <- length(midparent)
+	theta <- acos(par_off_correlation)
+	child <- rnorm(n)
+	X     <- cbind(midparent, child)         # matrix
+	Xctr  <- scale(X, center = TRUE, scale = FALSE)   # centered columns (mean 0)
+
+	Id   <- diag(n)                               # identity matrix
+	Q    <- qr.Q(qr(Xctr[ , 1, drop = FALSE]))      # QR-decomposition, just matrix Q
+	P    <- tcrossprod(Q)          # = Q Q'       # projection onto space defined by x1
+	x2o  <- (Id-P) %*% Xctr[ , 2]                 # x2ctr made orthogonal to x1ctr
+	Xc2  <- cbind(Xctr[ , 1], x2o)                # bind to matrix
+	Y    <- Xc2 %*% diag(1 / sqrt(colSums(Xc2^2)))  # scale columns to length 1
+
+	x <- Y[ , 2] + (1 / tan(theta)) * Y[ , 1]     # final new vector
+	print(cor(x, midparent))
+	
+	out <- list(midp = midparent, child = x)
+	return(out)
+}
+
+calculate_s <- function(dad_phen, mum_phen, par_off_correlation) {
+	phens <- generate_phens(dad_phen, mum_phen, par_off_correlation)
+	delt_mu <-  mean(phens$child) - mean(phens$midp)
+	regression <- least_squares_func(phens$child, phens$midp)
+	h2 <- regression$estimate
+	S <- delt_mu / h2
+	out <- list(S = S, h2 = h2, delt_mu = delt_mu)
+	return(out)
+}
+
+params <- expand.grid(
+	corr = c(0.001, 0.01, 0.1, 0.25, 0.5, 0.99),
+	S = NA, 
+	h2 = NA, 
+	delt_mu = NA
+	)
+
+set.seed(2)
+dp <- rnorm(1000)
+mp <- rnorm(1000)
+res <- map_dfr(split(params, 1:nrow(params)), function(x) {
+	out <- calculate_s(dp, mp, x$corr)
+	x$S = out$S
+	x$h2 = out$h2
+	x$delt_mu = out$delt_mu
+	return(x)
+})
+
+res
 
 
