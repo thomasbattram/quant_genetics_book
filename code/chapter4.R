80179ff427b0880e8a21179051cf978743c78625
Thomas Battram <tb13101@bristol.ac.uk>
2020-04-03 12:38:22 +0100

    updated chapter4 simulations with chisq test and lrt test for hwe

--- code/chapter4.R
+++ code/chapter4.R
@@ -1,5 +1,12 @@
 # Chapter 4
 
+pkgs <- c("tidyverse", "HardyWeinberg")
+lapply(pkgs, require, character.only = TRUE)
+
+# ---------------------------------------- 
+# chi-squared test -- tidy code!!!
+# ----------------------------------------
+
 b1b1 <- 300
 b1b2 <- 500
 b2b2 <- 200
@@ -11,21 +18,40 @@ exp_b1b1 <- ((b1)^2 / 1000)
 exp_b1b2 <- (2 * b1 * b2) / 1000
 exp_b2b2 <- (b2^2) / 1000
 
-tab <- as.table(rbind(c(exp_b1b1, exp_b1b2, exp_b2b2), c(b1b1, b1b2, b2b2)))
-dimnames(tab) <- list(data = c("expected", "observed"), 
-					  genotype = c("b1b1", "b1b2", "b2b2"))
-Xsq <- chisq.test(tab)
+chi2_part1 <- ((b1b1 - exp_b1b1)^2) / exp_b1b1
+chi2_part2 <- ((b1b2 - exp_b1b2)^2) / exp_b1b2
+chi2_part3 <- ((b2b2 - exp_b2b2)^2) / exp_b2b2
 
+chi2 <- sum(c(chi2_part1, chi2_part2, chi2_part3))
+
+# from package
 x <- c(b1b1, b1b2, b2b2)
-HW.test <- HWChisq(x,verbose=TRUE)
+HW.test <- HWChisq(x,verbose=TRUE, cc=0)
+
+# ---------------------------------------- 
+# lrt -- tidy code!!
+# ----------------------------------------
+
+lrt_hwe_test <- function(hom1_n, het_n, hom2_n) {
+	
+	b1 <- hom1_n + 0.5 * het_n
+	b2 <- hom2_n + 0.5 * het_n
+	exp_hom1_n <- ((b1)^2 / 1000)
+	exp_het_n <- (2 * b1 * b2) / 1000
+	exp_hom2_n <- (b2^2) / 1000
+
+	G <- -2 * ((hom1_n * log(exp_hom1_n / hom1_n)) +
+			het_n * log(exp_het_n / het_n) + 
+			hom2_n * log(exp_hom2_n / hom2_n))
+	p <- pchisq(G, df=1, lower.tail=FALSE)
+	out <- list(G = G, p_val = p)
+	return(out)
+}
+
+lrt_hwe_test(b1b1, b1b2, b2b2)
+HW.test2 <- HWExact(x,verbose=TRUE)
+# similar but not the same!
 
 
-     M <- as.table(rbind(c(762, 327, 468), c(484, 239, 477)))
-     dimnames(M) <- list(gender = c("F", "M"),
-                         party = c("Democrat","Independent", "Republican"))
-     (Xsq <- chisq.test(M))  # Prints test summary
 
-     Xsq$observed   # observed counts (same as M)
-     Xsq$expected   # expected counts under the null
-     Xsq$residuals  # Pearson residuals
-     Xsq$stdres     # standardized residuals
+
