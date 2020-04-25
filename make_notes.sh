ff22c7952bd79c55de47cbed9ddef9b534c944f6
Thomas Battram <tb13101@bristol.ac.uk>
2020-04-25 16:43:49 +0100

    updated note making

--- make_notes.sh
+++ make_notes.sh
@@ -7,7 +7,7 @@ chapters=$1
 if [[ $chapters == "all" ]]; then
 	Rscript -e "require(knitr); require(markdown); require(bookdown); bookdown::render_book('index.Rmd', output_format = 'all')"	
 else 
-	chap=03-chapter${chapters}
+	chap=0${chapters}-chapter${chapters}
 	echo $chap
 	# script="bookdown::preview_chapter('${chap}.Rmd')"
 	# echo $script
